5e68829e99d124baf42f37543ea275b33b0e60d2
Aline Cantarelli <aline.cantarelli@liferay.com>
2022-06-02 18:39:32 -0300

    LPS-154563 Create Modal add columns
    Co-authored-by: Milton Castro <milton.castro@liferay.com>

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectView/ModalAddColumns/ModalAddColumns.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectView/ModalAddColumns/ModalAddColumns.tsx
@@ -18,184 +18,56 @@ import ClayIcon from '@clayui/icon';
 import ClayList from '@clayui/list';
 import ClayModal from '@clayui/modal';
 import {ManagementToolbar} from 'frontend-js-components-web';
-import React, {FormEvent, useEffect, useState} from 'react';
+import React, {FormEvent, useMemo, useState} from 'react';
 
-import {defaultLanguageId} from '../../../utils/locale';
 import {ManagementToolbarSearch} from '../ManagementToolbarSearch/ManagementToolbarSearch';
-import {TYPES} from '../context';
 
 import './ModalAddColumns.scss';
-import {TObjectField} from '../types';
-interface IProps extends React.HTMLAttributes<HTMLElement> {
-	handleSubmit: any;
-	isActionBuilder?: boolean;
-	objectFields: any[];
-	objectViewColumns: any;
-	observer: any;
-	onClose: () => void;
-}
 
-const ModalAddColumns: React.FC<IProps> = ({
-	handleSubmit,
-	isActionBuilder,
-	objectFields,
-	objectViewColumns,
+function ModalAddColumns<T extends ModalItem>({
+	disableRequired,
+	getName,
+	items,
 	observer,
 	onClose,
-}) => {
-	const [checkedItems, setCheckedItems] = useState<unknown[]>(
-		objectViewColumns ?? []
-	);
+	onSave,
+	selected,
+}: IProps<T>) {
+	const [searchTerm, setSearchTerm] = useState('');
+	const [selectedItems, setSelectedItems] = useState(selected ?? []);
 
-	const [filteredItems, setFilteredItems] = useState(objectFields);
-	const [fieldsChecked, setFieldsChecked] = useState(false);
-	const [allFieldsChecked, setAllFieldsChecked] = useState(false);
-	const [query, setQuery] = useState('');
+	const filteredItems = useMemo(() => {
+		const loweredTerm = searchTerm.toLowerCase();
+		const selectedIds = new Set(selectedItems.map(({id}) => id));
 
-	useEffect(() => {
-		if (isActionBuilder && objectViewColumns) {
-			objectFields.forEach((objectField) => {
-				if (
-					objectViewColumns.find(
-						(requiedField: {id: string}) =>
-							requiedField.id === objectField.id
-					)
-				) {
-					objectField.checked = true;
-				}
-			});
-			setFilteredItems(objectFields);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+		return items
+			.filter((item) =>
+				getName(item)?.toLowerCase().includes(loweredTerm)
+			)
+			.map((item) => ({
+				...item,
+				checked:
+					disableRequired && item.required
+						? true
+						: selectedIds.has(item.id),
+			}));
+	}, [disableRequired, getName, searchTerm, selectedItems, items]);
 
-	useEffect(() => {
-		const checkedItems = filteredItems?.filter(
-			({checked}) => checked === true
-		);
+	const toggleFieldCheckbox = (id: unknown, checked: boolean) => {
+		if (checked) {
+			const selected = items.find((item) => item.id === id) as T;
 
-		setAllFieldsChecked(checkedItems?.length === filteredItems?.length);
-
-		setFieldsChecked(checkedItems?.length > 0);
-	}, [fieldsChecked, filteredItems]);
-
-	useEffect(() => {
-		setFilteredItems((filteredItems) =>
-			filteredItems?.map((field) => {
-				const filtered =
-					!query ||
-					field.label[defaultLanguageId]
-						.toLowerCase()
-						.includes(query.toLowerCase());
-
-				return {...field, filtered};
-			})
-		);
-	}, [query]);
-
-	const handleAllFieldsChecked = (checked: boolean) => {
-		setCheckedItems([]);
-
-		const setSelection = (checked: boolean) => {
-			if (allFieldsChecked) {
-				return false;
-			}
-			else if (fieldsChecked) {
-				return true;
-			}
-			else if (!checked) {
-				return false;
-			}
-			else {
-				setFieldsChecked(true);
-
-				return true;
-			}
-		};
-
-		const newFiltredItems = filteredItems?.map((field) => {
-			return {
-				...field,
-				checked: setSelection(checked),
-			};
-		});
-
-		setFilteredItems(newFiltredItems);
-
-		if (checked === true) {
-			setCheckedItems(
-				newFiltredItems.map((filteredItem, index) => {
-					return {
-						defaultSort: false,
-						fieldLabel: filteredItem.label[defaultLanguageId],
-						label: filteredItem.label,
-						objectFieldName: filteredItem.name,
-						priority: index,
-					};
-				})
-			);
+			setSelectedItems([...selectedItems, selected]);
 		}
 		else {
-			setCheckedItems([]);
+			setSelectedItems(selectedItems.filter((item) => item.id !== id));
 		}
-	};
-
-	const toggleFieldCheckbox = (name: string) => {
-		const newfilteredItems: TObjectField[] = [];
-
-		filteredItems?.map((field, index) => {
-			if (field.name === name) {
-				newfilteredItems.push({
-					...field,
-					checked: !field.checked,
-				});
-
-				if (!field.checked) {
-					setCheckedItems([
-						...checkedItems,
-						{
-							defaultSort: false,
-							fieldLabel: field.label[defaultLanguageId],
-							label: field.label,
-							objectFieldName: field.name,
-							priority: index,
-						},
-					]);
-				}
-				else {
-					const newCheckedItems = checkedItems.filter(
-						(checkedItem: any) =>
-							field.name !== checkedItem.objectFieldName
-					);
-					setCheckedItems(newCheckedItems);
-				}
-			}
-			else {
-				newfilteredItems.push(field);
-			}
-		});
-
-		setFilteredItems(newfilteredItems);
 	};
 
 	const onSubmit = (event: FormEvent) => {
 		event.preventDefault();
 
-		if (isActionBuilder) {
-			handleSubmit(
-				filteredItems.filter((field) => field.checked || field.required)
-			);
-		}
-		else {
-			handleSubmit({
-				payload: {
-					checkedItems,
-					filteredItems,
-				},
-				type: TYPES.ADD_OBJECT_VIEW_COLUMN,
-			});
-		}
-
+		onSave(selectedItems);
 		onClose();
 	};
 
@@ -217,57 +89,43 @@ const ModalAddColumns: React.FC<IProps> = ({
 					<ManagementToolbar.ItemList>
 						<ManagementToolbar.Item>
 							<ClayCheckbox
-								checked={fieldsChecked}
+								checked={items.length === selectedItems.length}
 								indeterminate={
-									!allFieldsChecked && fieldsChecked
+									selectedItems.length > 0 &&
+									items.length !== selectedItems.length
 								}
-								onChange={({target}) => {
-									if (!allFieldsChecked && fieldsChecked) {
-										handleAllFieldsChecked(true);
-									}
-									else {
-										handleAllFieldsChecked(target.checked);
-									}
+								onChange={() => {
+									const selected =
+										items.length === selectedItems.length
+											? []
+											: [...items];
+									setSelectedItems(selected);
 								}}
 							/>
 						</ManagementToolbar.Item>
 					</ManagementToolbar.ItemList>
 
 					<ManagementToolbarSearch
-						query={query}
-						setQuery={setQuery}
+						query={searchTerm}
+						setQuery={setSearchTerm}
 					/>
 				</ManagementToolbar.Container>
 			</ClayModal.Body>
 
 			<ClayForm onSubmit={(event) => onSubmit(event)}>
 				<ClayList className="lfr-object__object-view-modal-add-columns-list">
-					{filteredItems?.map((field, index) => (
-						<ClayList.Item
-							className={field?.filtered ? '' : 'hide'}
-							flex
-							key={`list-item-${index}`}
-						>
+					{filteredItems.map((item, index) => (
+						<ClayList.Item flex key={`list-item-${index}`}>
 							<ClayCheckbox
-								checked={
-									isActionBuilder
-										? field.required || field.checked
-										: field.checked
-								}
-								disabled={
-									isActionBuilder ? field.required : false
-								}
-								label={
-									isActionBuilder
-										? field.name
-										: field.label[defaultLanguageId]
-								}
+								checked={item.checked}
+								disabled={disableRequired && item.required}
+								label={getName(item)}
 								onChange={() => {
-									toggleFieldCheckbox(field.name);
+									toggleFieldCheckbox(item.id, !item.checked);
 								}}
 							/>
 
-							{isActionBuilder && field.required && (
+							{disableRequired && item.required && (
 								<span className="reference-mark">
 									<ClayIcon symbol="asterisk" />
 								</span>
@@ -295,6 +153,23 @@ const ModalAddColumns: React.FC<IProps> = ({
 			</ClayForm>
 		</ClayModal>
 	);
-};
+}
 
 export default ModalAddColumns;
+
+interface ModalItem {
+	id?: unknown;
+	label: LocalizedValue<string>;
+	required?: boolean;
+}
+
+interface IProps<T extends ModalItem>
+	extends React.HTMLAttributes<HTMLElement> {
+	disableRequired?: boolean;
+	getName: (label: T) => string;
+	items: T[];
+	observer: any;
+	onClose: () => void;
+	onSave: (selected: T[]) => void;
+	selected?: T[];
+}
