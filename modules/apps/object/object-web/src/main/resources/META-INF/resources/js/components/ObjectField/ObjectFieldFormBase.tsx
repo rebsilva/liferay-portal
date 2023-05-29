bf4a3863fd96ff0eef468ad512b04fcde998c361
Rebeca Silva <rebeca.silva@liferay.com>
2023-05-29 13:39:09 -0300

    Revert "LPS-182944 Add Feature Flag"
    This reverts commit caa08dae30afc9ea6308da5143fa4af5b5702c47.

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/ObjectFieldFormBase.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/ObjectFieldFormBase.tsx
@@ -478,16 +478,15 @@ export default function ObjectFieldFormBase({
 				</AutoComplete>
 			)}
 
-			{Liferay.FeatureFlags['LPS-143068'] &&
-				values.businessType === 'DateTime' && (
-					<TimeStorage
-						disabled={disabled}
-						objectFieldSettings={
-							values.objectFieldSettings as ObjectFieldSetting[]
-						}
-						setValues={setValues}
-					/>
-				)}
+			{values.businessType === 'DateTime' && (
+				<TimeStorage
+					disabled={disabled}
+					objectFieldSettings={
+						values.objectFieldSettings as ObjectFieldSetting[]
+					}
+					setValues={setValues}
+				/>
+			)}
 
 			{children}
 
