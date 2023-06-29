7d8a5392cb6294aa54f55c6c89c811073575c945
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-06-29 14:19:16 -0300

    LPS-188855 Create model resource to ObjectFolder model

--- modules/apps/object/object-api/src/main/java/com/liferay/object/constants/ObjectActionKeys.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/constants/ObjectActionKeys.java
@@ -23,6 +23,8 @@ public class ObjectActionKeys {
 
 	public static final String ADD_OBJECT_ENTRY = "ADD_OBJECT_ENTRY";
 
+	public static final String ADD_OBJECT_FOLDER = "ADD_OBJECT_FOLDER";
+
 	public static final String EXTEND_SYSTEM_OBJECT_DEFINITION =
 		"EXTEND_SYSTEM_OBJECT_DEFINITION";
 
