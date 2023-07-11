45aa426c08351a37ced780133e8fbea815bc3451
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-07-11 11:19:21 -0300

    LPS-188853 Implement methods in ObjectFolderServiceImpl

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFolderServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFolderServiceImpl.java
@@ -14,10 +14,21 @@
 
 package com.liferay.object.service.impl;
 
+import com.liferay.object.constants.ObjectActionKeys;
+import com.liferay.object.constants.ObjectConstants;
+import com.liferay.object.model.ObjectFolder;
 import com.liferay.object.service.base.ObjectFolderServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+
+import java.util.Locale;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Murilo Stodolni
@@ -30,4 +41,60 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class ObjectFolderServiceImpl extends ObjectFolderServiceBaseImpl {
+
+	@Override
+	public ObjectFolder addObjectFolder(
+			String externalReferenceCode, Map<Locale, String> labelMap,
+			String name)
+		throws PortalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), null, ObjectActionKeys.ADD_OBJECT_FOLDER);
+
+		return objectFolderLocalService.addObjectFolder(
+			externalReferenceCode, getUserId(), labelMap, name);
+	}
+
+	@Override
+	public ObjectFolder deleteObjectFolder(long objectFolderId)
+		throws PortalException {
+
+		_objectFolderModelResourcePermission.check(
+			getPermissionChecker(), objectFolderId, ActionKeys.DELETE);
+
+		return objectFolderLocalService.deleteObjectFolder(objectFolderId);
+	}
+
+	@Override
+	public ObjectFolder getObjectFolder(long objectFolderId)
+		throws PortalException {
+
+		_objectFolderModelResourcePermission.check(
+			getPermissionChecker(), objectFolderId, ActionKeys.VIEW);
+
+		return objectFolderLocalService.getObjectFolder(objectFolderId);
+	}
+
+	@Override
+	public ObjectFolder updateObjectFolder(
+			String externalReferenceCode, long objectFolderId,
+			Map<Locale, String> labelMap)
+		throws PortalException {
+
+		_objectFolderModelResourcePermission.check(
+			getPermissionChecker(), objectFolderId, ActionKeys.UPDATE);
+
+		return objectFolderLocalService.updateObjectFolder(
+			externalReferenceCode, objectFolderId, labelMap);
+	}
+
+	@Reference(
+		target = "(model.class.name=com.liferay.object.model.ObjectFolder)"
+	)
+	private ModelResourcePermission<ObjectFolder>
+		_objectFolderModelResourcePermission;
+
+	@Reference(target = "(resource.name=" + ObjectConstants.RESOURCE_NAME + ")")
+	private PortletResourcePermission _portletResourcePermission;
+
 }
