ec24b2c45ea8c769f6d49d6614ebaadf789beaf0
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-07-24 16:36:55 -0300

    LPS-191513 Remove index by objectFolderERC and add index by objectFolderId

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectDefinitionModelDocumentContributor.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectDefinitionModelDocumentContributor.java
@@ -15,17 +15,11 @@
 package com.liferay.object.internal.search.spi.model.index.contributor;
 
 import com.liferay.object.model.ObjectDefinition;
-import com.liferay.object.model.ObjectFolder;
-import com.liferay.object.service.ObjectFolderLocalService;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Carolina Barbosa
@@ -41,34 +35,12 @@ public class ObjectDefinitionModelDocumentContributor
 	public void contribute(
 		Document document, ObjectDefinition objectDefinition) {
 
-		try {
-			ObjectFolder objectFolder =
-				_objectFolderLocalService.getObjectFolder(
-					objectDefinition.getObjectFolderId());
-
-			document.addKeyword(
-				"objectFolderExternalReferenceCode",
-				objectFolder.getExternalReferenceCode());
-
-			document.addLocalizedKeyword(
-				"localized_label", objectDefinition.getLabelMap(), true, true);
-			document.addText(Field.NAME, objectDefinition.getShortName());
-			document.remove(Field.USER_NAME);
-		}
-		catch (PortalException portalException) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Unable to index object definition " +
-						objectDefinition.getObjectDefinitionId(),
-					portalException);
-			}
-		}
+		document.addText(Field.NAME, objectDefinition.getShortName());
+		document.addLocalizedKeyword(
+			"localized_label", objectDefinition.getLabelMap(), true, true);
+		document.addKeyword(
+			"objectFolderId", objectDefinition.getObjectFolderId());
+		document.remove(Field.USER_NAME);
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		ObjectDefinitionModelDocumentContributor.class);
-
-	@Reference
-	private ObjectFolderLocalService _objectFolderLocalService;
 
 }
