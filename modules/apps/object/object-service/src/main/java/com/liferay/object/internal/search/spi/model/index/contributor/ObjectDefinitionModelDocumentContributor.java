531b781cbff73061d5b60ab81bb0e7dceb49642b
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-07-24 16:36:55 -0300

    LPS-191513 Add index by objectFolderId

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectDefinitionModelDocumentContributor.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectDefinitionModelDocumentContributor.java
@@ -26,9 +26,11 @@ public class ObjectDefinitionModelDocumentContributor
 	public void contribute(
 		Document document, ObjectDefinition objectDefinition) {
 
+		document.addText(Field.NAME, objectDefinition.getShortName());
 		document.addLocalizedKeyword(
 			"localized_label", objectDefinition.getLabelMap(), true, true);
-		document.addText(Field.NAME, objectDefinition.getShortName());
+		document.addKeyword(
+			"objectFolderId", objectDefinition.getObjectFolderId());
 		document.remove(Field.USER_NAME);
 	}
 
