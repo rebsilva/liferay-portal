e37ed7ed9b60c22185fc19ec1ac414f7089a9c0f
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-07-25 15:46:59 -0300

    LPS-188853 buildService

--- modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectFolderLocalService.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectFolderLocalService.java
@@ -1,15 +1,6 @@
 /**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.object.service;
@@ -211,6 +202,9 @@ public interface ObjectFolderLocalService
 	public ObjectFolder fetchObjectFolder(long objectFolderId);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public ObjectFolder fetchObjectFolder(long companyId, String name);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public ObjectFolder fetchObjectFolderByExternalReferenceCode(
 		String externalReferenceCode, long companyId);
 
@@ -244,6 +238,10 @@ public interface ObjectFolderLocalService
 	 */
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public ObjectFolder getObjectFolder(long objectFolderId)
+		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public ObjectFolder getObjectFolder(long companyId, String name)
 		throws PortalException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/persistence/test/ObjectFolderPersistenceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/persistence/test/ObjectFolderPersistenceTest.java
@@ -1,15 +1,6 @@
 /**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.object.service.persistence.test;
@@ -216,15 +207,6 @@ public class ObjectFolderPersistenceTest {
 		_persistence.countByUuid_C("null", 0L);
 
 		_persistence.countByUuid_C((String)null, 0L);
-	}
-
-	@Test
-	public void testCountByName() throws Exception {
-		_persistence.countByName("");
-
-		_persistence.countByName("null");
-
-		_persistence.countByName((String)null);
 	}
 
 	@Test
@@ -540,12 +522,6 @@ public class ObjectFolderPersistenceTest {
 	}
 
 	private void _assertOriginalValues(ObjectFolder objectFolder) {
-		Assert.assertEquals(
-			objectFolder.getName(),
-			ReflectionTestUtil.invoke(
-				objectFolder, "getColumnOriginalValue",
-				new Class<?>[] {String.class}, "name"));
-
 		Assert.assertEquals(
 			Long.valueOf(objectFolder.getCompanyId()),
 			ReflectionTestUtil.<Long>invoke(
