1d90bb229ee75327f96aa69ecd4479ee4ceeec10
Rebeca Silva <rebeca.silva@liferay.com>
2025-05-27 13:10:13 -0300

    do not use this commit

---
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/EditFriendlyURL.tsx
@@ -0,0 +1,94 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayAlert from '@clayui/alert';
+import ClayButton from '@clayui/button';
+import ClayForm from '@clayui/form';
+import ClayModal, {useModal} from '@clayui/modal';
+import { openToast } from 'frontend-js-components-web';
+import React, {useState} from 'react';
+import { SeparatorContainer } from './ObjectDetails/SeparatorContainer';
+import { API } from '@liferay/object-js-components-web';
+import { Error, handleErrors } from '../utils/errors';
+
+interface ModalEditObjectDefinitionExternalReferenceCodeProps {
+	handleOnClose: () => void;
+    setValues: (values: Partial<ObjectDefinition>) => void;
+	values: Partial<ObjectDefinition>;
+}
+
+export function ModalEditObjectDefinitionExternalReferenceCode({
+	handleOnClose,
+    setValues,
+	values,
+}: ModalEditObjectDefinitionExternalReferenceCodeProps) {
+	const [errors, setErrors] = useState<Error>({});
+	const {observer, onClose} = useModal({
+		onClose: () => {
+			handleOnClose();
+		},
+	});
+
+	const onSubmit = async () => {
+
+        let objectDefinition = values;
+
+		const saveResponse =
+                        await API.putObjectDefinitionByExternalReferenceCode(objectDefinition);
+        
+                    if (!saveResponse.ok) {
+                        const {detail, title} = (await saveResponse.json()) as Error;
+        
+                        handleErrors({detail, title}, setErrors);
+                        return;
+                    }
+
+                    onClose();
+
+                    openToast({
+                                    message: Liferay.Language.get(
+                                        'the-object-was-saved-successfully'
+                                    ),
+                                    type: 'success',
+                    });
+		
+		
+	};
+
+	return (
+		<ClayModal center observer={observer}>
+			<ClayForm >
+				<ClayModal.Header>
+					{Liferay.Util.sub(
+						Liferay.Language.get('edit-x'),
+						Liferay.Language.get('entry-url-separator')
+					)}
+				</ClayModal.Header>
+
+				<ClayModal.Body>
+					<SeparatorContainer errors={errors} values={values} setValues={setValues} ></SeparatorContainer>
+					
+				</ClayModal.Body>
+
+				<ClayModal.Footer
+					last={
+						<ClayButton.Group key={1} spaced>
+							<ClayButton
+								displayType="secondary"
+								onClick={onClose}
+							>
+								{Liferay.Language.get('cancel')}
+							</ClayButton>
+
+							<ClayButton displayType="primary" onClick={onSubmit} type="submit">
+								{Liferay.Language.get('save')}
+							</ClayButton>
+						</ClayButton.Group>
+					}
+				/>
+			</ClayForm>
+		</ClayModal>
+	);
+}
---
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/FriendlyURL/SeoContainer.tsx
@@ -0,0 +1,65 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayButton from '@clayui/button';
+import ClayForm from '@clayui/form';
+import ClayIcon from '@clayui/icon';
+import {useModal} from '@clayui/modal';
+import React, {useState} from 'react';
+
+import {AllowFriendlyURLContainer} from '../../ObjectDetails/AllowFriendlyURLContainer';
+
+import './SeoContainer.scss';
+import {useObjectFolderContext} from '../ModelBuilderContext/objectFolderContext';
+
+interface SeoContainerProps {
+	onSubmit?: (editedObjectDefinition?: Partial<ObjectDefinition>) => void;
+	setValues: (values: Partial<ObjectDefinition>) => void;
+	values: Partial<ObjectDefinition>;
+}
+
+export function SeoContainer({onSubmit, setValues, values}: SeoContainerProps) {
+
+	const [{modelBuilderModals}, dispatch] = useObjectFolderContext();
+
+	const [visibleModal, setVisibleModal] = useState(false);
+
+	const {observer, onClose} = useModal({
+		onClose: () => setVisibleModal(false),
+	});
+
+	// lembrar de colocar no final da url o friendly separator
+
+	return (
+		<ClayForm.Group>
+			<div className='lfr-objects__model-builder-seo-container-header'>
+				<label className="mb-0">
+					{Liferay.Language.get('object-entry-url-separator')}
+				</label>
+
+				<p className="mb-1 small text-secondary">
+					http://www.liferay.com
+				</p>
+			</div>
+
+			<ClayButton
+				aria-label={Liferay.Language.get('show-actions')}
+				className= 'lfr-objects__model-builder-seo-container-edit-url-button'
+				displayType="secondary"
+				onClick={() => setVisibleModal(true)}
+			>
+				<ClayIcon className='lfr-objects__model-builder-seo-container-button-icon' symbol="link" />
+
+				{Liferay.Language.get('edit-entry-url-separator')}
+			</ClayButton>
+
+			<AllowFriendlyURLContainer
+				onSubmit={onSubmit}
+				setValues={setValues}
+				values={values}
+			/>
+		</ClayForm.Group>
+	);
+}
